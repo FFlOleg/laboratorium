bf8ef25de812361de3160f00b0061a0cbe4638fb
Oleg <olegflak35@gmail.com>
2025-03-14 16:23:15 +0100

    Delete django/djangotutorial directory

--- django/djangotutorial/mysite/mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # импортируем include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),  # подключаем маршруты из приложения polls
-]
